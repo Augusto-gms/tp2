3e484e1227dc69da3fcf61ea7a7ac5516d8df96c
Augusto-gms <gutogpaassuncao@gmail.com>
2026-04-15 15:22:23 -0300

    class Restaurante

--- java/Data.java
+++ java/Data.java
@@ -4,14 +4,14 @@ public class Data{
   private int ano;
   private int mes;
   private int dia;
-  public Data(int ano, int dia, int ano){
+  public Data(int dia, int mes, int ano){
     this.dia = dia;
     this.mes = mes;
     this.ano = ano;
   }
   int getAno(){return this.ano;}
   int getMes(){return this.mes;}
-  int getDia(){return this.diaia;}
+  int getDia(){return this.dia;}
   public static parseData(String s){
     Scanner sc = new Scanner(s);
 
--- java/Restauranetes.java
+++ java/Restauranetes.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class Restaurantes{
-  //variavaeis privadas
+public class Restaurante{
+  // variáveis privadas
   private int id;
   private String nome;
   private String cidade;
@@ -13,69 +13,79 @@ public class Restaurantes{
   private Hora horarioFechamento;
   private Data dataAbertura;
   private boolean aberto;
-  //metodo construtor
-  public Restaurantes(int id, String nome, String cidade, int capacidade, double avaliacao, String tiposCozinha[], int faixaPreco, Hora horarioAbertura, Hora horarioFechamento, Data dataAbertura, boolean aberto){
-      this id = id;
-      this.nome = nome;
-      this.cidade = cidade;
-      this.capacidade = capacidade;
-      this.avaliacao = avaliacao;
-      this.tiposCozinha = tiposCozinha;
-      this.faixaPreco = faixaPreco;
-      this.horarioAbertura = horarioAbertura;
-      this.horarioFechamento = horarioFechamento;
-      this.dataAbertura = dataAbertura;
-      this.aberto = aberto;
-  }
-  int getId(){return this.id;}
-  String getNome(){return this.nome;}
-  String getCidade(){return this.cidade;}
-  int getCapacidade(){return this.capacidade;}//fzr set
-  double getAvaliacao(){return this.avaliacao;}//fzr set
-  String getTiposCozinha[](){return this.tiposCozinha;}//fzr set
-  int getFaixaPreco(){return this.faixaPreco;}//fzr set
-  Hora getHorarioAbertura(){return this.horarioAbertura;}//fzr set
-  Hora getHorarioFechamento(){return this.horarioFechamento;}//fzr set
-  Data getDataAbertura(){return this.dataAbertura;}
-  boolean getAberto(){return this.aberto;}//fzr set
 
-  void setCapacidade(int capacidade){
-      this.capacidade = capacidade; 
+  //construtor
+  public Restaurante(int id, String nome, String cidade, int capacidade, double avaliacao, String tiposCozinha[], int faixaPreco, Hora horarioAbertura, Hora horarioFechamento, Data dataAbertura, boolean aberto) {
+    this.id = id;
+    this.nome = nome;
+    this.cidade = cidade;
+    this.capacidade = capacidade;
+    this.avaliacao = avaliacao;
+    this.tiposCozinha = tiposCozinha;
+    this.faixaPreco = faixaPreco;
+    this.horarioAbertura = horarioAbertura;
+    this.horarioFechamento = horarioFechamento;
+    this.dataAbertura = dataAbertura;
+    this.aberto = aberto;
   }
-  void setAvaliacao(double avaliacao){
-      if(avaliacao > 5 || avaliacao < 0){
-        System.out.println("erroAvaliacao");
-      }else{
-        this.avaliacao = avaliacao;
-      }
+
+  //getters
+  public int getId() { return this.id; }
+  public String getNome() { return this.nome; }
+  public String getCidade() { return this.cidade; }
+  public int getCapacidade() { return this.capacidade; }
+  public double getAvaliacao() { return this.avaliacao; }
+  public String[] getTiposCozinha() { return this.tiposCozinha; }
+  public int getFaixaPreco() { return this.faixaPreco; }
+  public Hora getHorarioAbertura() { return this.horarioAbertura; }
+  public Hora getHorarioFechamento() { return this.horarioFechamento; }
+  public Data getDataAbertura() { return this.dataAbertura; }
+  public boolean getAberto() { return this.aberto; }
+
+  //setters
+  public void setCapacidade(int capacidade){
+    this.capacidade = capacidade;
   }
-  void setTiposCozinha(String tiposCozinha[]){
+  public void setAvaliacao(double avaliacao){
+    if(avaliacao > 0 && avaliacao < 5){
+      this.avaliacao = avaliacao;
+    }else{
+      System.out.println("erroAvaliacao");
+    }
+  }
+  public void setTiposCozinha(String tiposCozinha[]){
     this.tiposCozinha = tiposCozinha;
   }
-  void setFaixaPreco(int faixaPreco){
-      if(faixaPreco.length() > 4 || faixaPreco.length() < 1){
-        System.out.println("erroFaixaPreco");
-      }else{
-        this.faixaPreco = faixaPreco;
-      }
+  public void setFaixaPreco(int faixaPreco){
+    this.faixaPreco = faixaPreco;
   }
-}
-public void setHorarioAbertura(Hora horarioAbertura){
-	this.horarioAbertura = horarioAbertura;
-}
-public void setHorarioFechamento(Hora horarioFechamento){
-	this.horarioFechado = horariFechamento;
-}
-public void setAberto(boolean aberto){
-	this.aberto = aberto;
-}
+  public void setHorarioAbertura(Hora horarioAbertura){
+    this.horarioAbertura = horarioAbertura;
+  }
+  public void setHorarioFechamento(Hora horarioFechamento){
+    this.horarioFechamento = horarioFechamento;
+  }
+  public void setAberto(boolean aberto){
+    this.aberto = aberto;
+  }
+  public static Restaurante parseRestaurante(String s){
+    Scanner sc = new Scanner(s);
+    sc.useDelimiter(",");
+    
+    int id = sc.nextInt();
+    String nome = sc.next();
+    String cidade = sc.next();
+    int capacidade = sc.nextInt();
+    double avaliacao = sc.nextDouble();
+    String tiposCozinha = sc.next();
+    int faixaPreco = sc.nextInt();
 
-public static Restaurante parseRestaurante(String s){
-	Scanner sc = new Scanner(s);
+    Hora abertura = Hora.parseHora(sc.next());
+    Hora fechamento = Hora.parseHora(sc.next());
+    Data data = Data.parseData(sc.next());
 
-	sc.useDelimiter(",");
-
-	while(sc.hasNextLine()){
-		setCapacidade(s);
-	}
+    boolean aberto = sc.nextBoolean();
+    sc.close();
+    return new Restaurante(id,nome,cidade,capacidade,avaliacao,tiposCozinha,faixaPreco,abertura,fechamento,data,aberto);
+  }
 }
